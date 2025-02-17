92ba181e2d62cf1236c2e2806270ec44d2b7d2f8
twonkista <rkomati787@gmail.com>
2025-02-17 09:04:50 -0600

    final changes + more testcases

--- proj2-search/search.cpp
+++ proj2-search/search.cpp
@@ -16,13 +16,18 @@ string cleanLeftToken(const string& token);
 
 string cleanToken(const string& token) {
   // TODO student
-  int end = token.length();  // end index
+  //int end = token.length();  // end index
 
   if(token.length() == 1) {
-    char new_token = tolower(token[0]);                               //bro suck me off thats how good i am
-    string sds = "";
-    sds += new_token;
-    return sds;
+    if(isalpha(token[0])) {
+      char new_token = tolower(token[0]);                               
+      string sds = "";
+      sds += new_token;
+      return sds;
+    }
+    else {
+      return "";
+    }
   }
   else if (token == "") {
     return token;
@@ -30,7 +35,7 @@ string cleanToken(const string& token) {
 
   string remo_front = cleanRightToken(token);
   string remo_back = cleanLeftToken(remo_front);
-  //BRO SMDFR U PMO
+
   string sting = "";
 
   int count = 0;
@@ -64,12 +69,12 @@ string cleanRightToken(const string& token) {
     }
   }
 
-  return token.substr(leading, token.length());
+  return token.substr(leading);
 }
 
 string cleanLeftToken(const string& token) {
   int trailing = token.length();
-  for(int i = token.length()-1; i >= 0; --i) {
+  for(int i = token.length()-1; i >= 0; i--) {
     if(ispunct(token[i])) {
       trailing--;
     }
@@ -86,29 +91,127 @@ set<string> gatherTokens(const string& text) {
   // TODO student
   set<string> gatherWords;
   string element = "";
-  for (int i = 0; i <= text.size(); i++) {
-    if ((isalpha(text[i]) && i < text.size()) || (ispunct(text[i]) && i < text.size() && isalpha(text[i-1]) && i != 0 && i != text.size()-1 && isalpha(text[i+1]))) {  // alpha and not the last element or if the punctuation isnt the last or first, element and the element before is a letter
-      element += tolower(text[i]);                    // add to the string
-    } else if (element != "") { //too long didnt read
-      gatherWords.insert(element);  // push to vector
-      element = "";            // resets the element string
+  for(int i = 0; i < text.length(); i++) {
+    if(!isspace(text[i])){
+      element += text[i];
+    }
+    else {
+      gatherWords.insert(cleanToken(element));
+      element = "";
     }
   }
+  gatherWords.insert(cleanToken(element));
+  gatherWords.erase("");
   return gatherWords;
-  //micky mouse i gotta fix later
 }
 
 int buildIndex(const string& filename, map<string, set<string>>& index) {
   // TODO student
-  return 0;
+  
+  ifstream infile(filename);
+  if(!infile.is_open()){
+    return 0;
+  }
+  string url;
+  int url_count = 0;
+  string base;
+
+  while(!infile.eof()) {
+    getline(infile, url);
+    getline(infile, base);
+    if(!infile.fail()) {
+      string sentence = cleanToken(base);
+      set <string> words = gatherTokens(sentence);
+      for(auto word: words) {
+          index[word].insert(url);
+      }
+      url_count++;
+    }
+  }
+  return url_count;
 }
 
 set<string> findQueryMatches(const map<string, set<string>>& index,
                              const string& sentence) {
   // TODO student
-  return {};
+  map<string,set<string>> copy = index; //copy of index map
+  vector<string> word_list; // list of words
+  string word = "";
+  set<string> resultant; //resultant set
+
+  //iterate through sentence
+  for(int i = 0; i < sentence.length(); i++) {
+    if(isalpha(sentence[i]) || sentence[i] == '+' || sentence[i] == '-') {
+      word += tolower(sentence[i]);
+    }
+    else if(word != "") {
+      word_list.push_back(word);
+      word = "";
+    }
+  }
+
+  if (!word.empty()) { 
+    word_list.push_back(word); 
+  }
+
+  for(int j = 0; j < word_list.size(); j++) { //iterate through the vector of words
+    string search = ""; // the term for the word
+    if(word_list[j][0] == '+'|| word_list[j][0] == '-') {
+      search = word_list[j].substr(1); //just the word
+    }
+    else {
+      search = word_list[j];
+    }
+
+    set<string> search_res = copy[search]; // only the values of the key
+
+    set<string> temp;
+    if(word_list[j][0] == '+') {
+      set_intersection(resultant.begin(), resultant.end(), search_res.begin(), search_res.end(), inserter(temp, temp.begin()));
+      resultant = temp;
+    }
+    else if(word_list[j][0] == '-') {
+      set_difference(resultant.begin(), resultant.end(), search_res.begin(), search_res.end(), inserter(temp, temp.begin()));
+      resultant = temp;
+    }
+    else {
+      set_union(resultant.begin(), resultant.end(), search_res.begin(), search_res.end(), inserter(temp, temp.end()));
+      resultant = temp;
+    }
+  }
+
+  return resultant;
 }
 
 void searchEngine(const string& filename) {
   // TODO student
+  map<string, set<string>> index;
+  int indexSize = buildIndex(filename,index);
+  if(indexSize == 0) {
+    cout << "Invalid filename." << endl;
+    cout<<"Stand by while building index..."<<endl;
+    cout<<"Indexed 0 pages containing 0 unique terms"<<endl;
+  }
+  else {
+    cout<<"Stand by while building index..."<<endl;
+    cout << "Indexed ";
+    cout << to_string(indexSize);
+    cout << " pages containing "; 
+    cout << index.size(); 
+    cout << " unique terms" << endl;
+  }
+  while(true) {
+    cout << "Enter query sentence (press enter to quit): ";
+    string command;
+    getline(cin, command);
+    if (command == "") {
+      break;
+    }
+    set<string> result = findQueryMatches(index, command);
+    cout << "Found "+ to_string(result.size()) + " matching pages" << endl;
+    for(auto i: result) {
+      cout << i << endl;
+    }
+  }
+  cout << "Thank you for searching!";
 }
--- proj2-search/search_tests.cpp
+++ proj2-search/search_tests.cpp
@@ -6,26 +6,170 @@
 using namespace std;
 using namespace testing;
 
-TEST(CleanToken, OneLetter) {
-  string token = "D";
-  string res_token = "d";
-  EXPECT_THAT(cleanToken(token), StrEq(res_token))
-      << "token=\"" << token << "\"";
-
+TEST(CleanToken, OneChar) {
+  ASSERT_THAT(cleanToken("A"), StrEq("a"));
+  ASSERT_THAT(cleanToken("+"), StrEq(""));
+  ASSERT_THAT(cleanToken("B"), StrEq("b"));
 }
 
-TEST(CleanToken, PunctInBetween) {
-  string token = ",,,hi,,there,,,";
-  string res_token = "hi,,there";
-  EXPECT_THAT(cleanToken(token), StrEq(res_token))
-      << "token=\"" << token << "\"";
-
+TEST(CleanToken, PunctStartEnd) {
+  ASSERT_THAT(cleanToken(",,hithere,,"), StrEq("hithere"));
+  ASSERT_THAT(cleanToken("!!!!youfool!!!"), StrEq("youfool"));
+  ASSERT_THAT(cleanToken("...ethan..."), StrEq("ethan"));
 }
 
-TEST(CleanToken, NumLetter) {
-  string token = ",,,223A";
-  string res_token = "223a";
-  EXPECT_THAT(cleanToken(token), StrEq(res_token))
-      << "token=\"" << token << "\"";
-
+TEST(CleanToken, PunctInMiddle) {
+  ASSERT_THAT(cleanToken("hi,,there"), StrEq("hi,,there"));
+  ASSERT_THAT(cleanToken("yeat.....andcart"), StrEq("yeat.....andcart"));
+  ASSERT_THAT(cleanToken("New..York..City"), StrEq("new..york..city"));
 }
+
+TEST(CleanToken, MiddleStart) {
+  ASSERT_THAT(cleanToken(",,,hi,(&%(7)@($)),there"), StrEq("hi,(&%(7)@($)),there"));
+  ASSERT_THAT(cleanToken(",,,you!!!fool"), StrEq("you!!!fool"));
+  ASSERT_THAT(cleanToken(",,,make...files"), StrEq("make...files"));
+}
+
+TEST(CleanToken, MiddleEnd) {
+  ASSERT_THAT(cleanToken("hi,(&%(7)@($)),there,,,"), StrEq("hi,(&%(7)@($)),there"));
+  ASSERT_THAT(cleanToken("you!!!fool,,,"), StrEq("you!!!fool"));
+  ASSERT_THAT(cleanToken("make...files,,,"), StrEq("make...files"));
+}
+
+TEST(GatherTokens, LeadingSpaces) {
+  string token = "   hi how are you?";
+  set<string> res_set = {"hi", "how", "are","you"};
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+  token = "     hi how are you?";
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+  token = "   where are my birkenstocks?";
+  res_set = {"where", "are", "my","birkenstocks"};
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+}
+
+TEST(GatherTokens, TrailingSpaces) {
+  string token = "hi how are you?   ";
+  set<string> res_set = {"hi", "how", "are","you"};
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+  token = "hi how are you?       ";
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+  token = "where are my birkenstocks?       ";
+  res_set = {"where", "are", "my","birkenstocks"};
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+}
+
+TEST(GatherTokens, SpacesInBetween) {
+  string token = "hi   how     are      you?";
+  set<string> res_set = {"hi", "how", "are","you"};
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+  token = "new      york     city";
+  res_set = {"new", "york", "city"};
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+  token = "four   five   six   train   east     harlem";
+  res_set = {"four", "five", "six", "train", "east", "harlem"};
+  ASSERT_THAT(gatherTokens(token), ContainerEq(res_set));
+}
+
+TEST(BuildIndex, TinyTxt) {
+  string filename = "data/tiny.txt";
+  map<string, set<string>> expectedIndex = {
+      {"eggs", {"www.shoppinglist.com"}},
+      {"milk", {"www.shoppinglist.com"}},
+      {"fish", {"www.shoppinglist.com","www.dr.seuss.net"}},
+      {"bread", {"www.shoppinglist.com"}},
+      {"cheese", {"www.shoppinglist.com"}},
+      {"red", {"www.rainbow.org",{"www.dr.seuss.net"}}},
+      {"gre-en", {"www.rainbow.org"}},
+      {"orange", {"www.rainbow.org"}},
+      {"yellow", {"www.rainbow.org"}},
+      {"blue", {"www.rainbow.org","www.dr.seuss.net"}},
+      {"indigo", {"www.rainbow.org"}},
+      {"violet", {"www.rainbow.org"}},
+      {"one", {"www.dr.seuss.net"}},
+      {"two", {"www.dr.seuss.net"}},
+      {"i'm", {"www.bigbadwolf.com"}},
+      {"not", {"www.bigbadwolf.com"}},
+      {"trying", {"www.bigbadwolf.com"}},
+      {"to", {"www.bigbadwolf.com"}},
+      {"eat", {"www.bigbadwolf.com"}},
+      {"you", {"www.bigbadwolf.com"}},
+  };
+  map<string, set<string>> studentIndex;
+  int studentNumProcesed = buildIndex(filename, studentIndex);
+
+  string indexTestFeedback =
+      "buildIndex(\"" + filename + "\", ...) index incorrect\n";
+  EXPECT_THAT(studentIndex, ContainerEq(expectedIndex)) << indexTestFeedback;
+
+  string retTestFeedback =
+      "buildIndex(\"" + filename + "\", ...) return value incorrect\n";
+  EXPECT_THAT(studentNumProcesed, Eq(4)) << retTestFeedback;
+}
+
+TEST(BuildIndex, InvalidFile) {
+  string filename = "ethan.txt";
+  map<string, set<string>> expected;
+  int count = buildIndex(filename, expected);
+  EXPECT_TRUE(expected.empty());
+}
+
+map<string, set<string>> INDEX = {
+    {"hello", {"example.com", "uic.edu"}},
+    {"there", {"example.com"}},
+    {"according", {"uic.edu"}},
+    {"to", {"uic.edu"}},
+    {"all", {"example.com", "uic.edu", "random.org"}},
+    {"known", {"uic.edu"}},
+    {"laws", {"random.org"}},
+    {"of", {"random.org"}},
+    {"mohan", {"mohan.org","uic.edu"}},
+    {"dizzler",{"uic.edu","random.org"}},
+    {"desai", {"luckydsgamingcafe.com"}},
+    {"dating", {"luckydsgamingcafe.com", "random.org"}},
+    {"aviation", {"random.org"}},
+    {"a", {"uic.edu", "random.org"}},
+    {"no", {"uic.edu"}},
+};
+
+TEST(FindQueryMatches, FirstItemNoExist) {
+  set<string> expected;
+
+  expected = {"uic.edu","random.org"};
+  EXPECT_THAT(findQueryMatches(INDEX, "ethan known to aviation"),
+              ContainerEq(expected));
+  
+  expected = {"random.org","luckydsgamingcafe.com"};
+  EXPECT_THAT(findQueryMatches(INDEX, "harsh desai laws of dating"),
+              ContainerEq(expected));
+}
+
+TEST(FindQueryMatches, LaterItemNoExist) {
+  set<string> expected;
+
+  expected = {"uic.edu"};
+  EXPECT_THAT(findQueryMatches(INDEX, "dizzler +mohan -aura"),
+              ContainerEq(expected));
+  
+  expected = {"mohan.org","uic.edu"};
+  EXPECT_THAT(findQueryMatches(INDEX, "known to all +greatest mohan"),
+              ContainerEq(expected));
+
+  expected = {"random.org","uic.edu"};
+  EXPECT_THAT(findQueryMatches(INDEX, "laws of no play according to dizzler"),
+              ContainerEq(expected));
+}
+
+// TEST(FindQueryMatches, LongCombinedQueries) {
+//   set<string> expected;
+
+//   expected = {"example.com"};
+//   EXPECT_THAT(findQueryMatches(INDEX, "there laws aviation -to +hello"),
+//               ContainerEq(expected));
+
+//   expected = {};
+//   EXPECT_THAT(findQueryMatches(INDEX, "all -all +all"), ContainerEq(expected));
+
+//   expected = {"example.com", "uic.edu", "random.org"};
+//   EXPECT_THAT(findQueryMatches(INDEX, "all -all +all all"),
+//               ContainerEq(expected));
+// }
